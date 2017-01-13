2ed7e8f85042a9f4bfede09def9b11f494eba1ed
Joao Roberto Lima <jbetol@gmail.com>
2017-01-13 09:52:15 -0200

    Starting with images/paperclip

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,6 @@
 module ArticlesHelper
+# defining which fields the model is dealing with. To avoid mass assignment
 	def article_params
-		params.require(:article).permit(:title, :body, :tag_list)
+		params.require(:article).permit(:title, :body, :tag_list, :image)
 	end	
 end
