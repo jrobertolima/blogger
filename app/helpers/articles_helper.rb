79a71a686f43c92b70891bc604f47f96dc88695a
jrobertolima <jbetol@gmail.com>
2017-01-13 11:42:24 -0200

    finished paperclip gem feature

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,7 @@
 module ArticlesHelper
 # defining which fields the model is dealing with. To avoid mass assignment
-	def article_params
-		params.require(:article).permit(:title, :body, :tag_list, :image)
-	end	
+#I'm using a private method in articles_controller
+#	def article_params
+#		params.require(:article).permit(:title, :body, :tag_list, :image)
+#	end	
 end
--- db/migrate/20170112203023_add_paperclip_fields_to_article.rb
+++ db/migrate/20170112203023_add_paperclip_fields_to_article.rb
@@ -3,6 +3,6 @@ class AddPaperclipFieldsToArticle < ActiveRecord::Migration[5.0]
 	add_column :articles, :image_file_name, 	:string
 	add_column :articles, :image_content_type,	:string 
 	add_column :articles, :image_file_size,		:integer 
-	add_column :articles, :image_updated_at,		:datetime
+	add_column :articles, :image_updated_at,	:datetime
   end
 end
